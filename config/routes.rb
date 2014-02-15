5855c43486272de918247475fade45660e220789
Lionuser <lionuser@codelion.net>
2014-02-15 08:25:47 +0000

    라우트 지정하고 보안 설정 해제하기

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Timeline::Application.routes.draw do
-  get "wall/write"
-  get "wall/posts"
+  get "wall/write" => 'wall#write'
+  post "wall/posts" => 'wall#posts'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
