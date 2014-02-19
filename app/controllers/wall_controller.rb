2f0b8e6d6ff77ef9ac6ccc49aa68f6220b5554aa
Lionuser <lionuser@codelion.net>
2014-02-19 07:28:17 +0000

    타임라인 글 저장 및 화면 전환 기능 추가하기

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -3,6 +3,15 @@ class WallController < ApplicationController
   end
 
 	def write_complete
+		p = Post.new
+		p.name = params[:writer]
+		p.content = params[:content]
+		if p.save
+			redirect_to "/wall/posts"
+		else
+			flash[:alert] = p.errors[:content][0]
+			redirect_to :back
+		end
 	end
 
   def posts
