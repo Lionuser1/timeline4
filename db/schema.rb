be538251400b4e4dd3fbb9c1f09fd031c6531018
Lionuser <lionuser@codelion.net>
2014-02-19 08:46:31 +0000

    마이그레이션 파일과 모델 파일 작성하고 댓글 테이블 만들기

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219064238) do
+ActiveRecord::Schema.define(version: 20140219084341) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "post_id"
+    t.string   "name"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "name"
