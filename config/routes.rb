06f8497a524b742307e0c982456abeed1026f786
Lionuser <lionuser@codelion.net>
2014-02-19 07:58:46 +0000

    라우트 지정하기

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Timeline::Application.routes.draw do
  	get "wall/posts"
 	get "wall/edit/:id" => 'wall#edit'
 	post "wall/edit_complete"
+	get "wall/delete/:id" => 'wall#delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
